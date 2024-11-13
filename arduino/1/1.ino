580332969adc6b7d3e7cb511fcb8b9d97b95fc6a
Bastian Medina <basy.medy@uc.cl>
2024-11-12 22:17:49 -0300

    Serial

--- arduino/1/1.ino
+++ arduino/1/1.ino
@@ -1,18 +1,29 @@
-int number = 2;  // Start with 2
-
 void setup() {
-  pinMode(7, INPUT_PULLUP);  // Set pin 7 as input with pull-up resistor
-  Serial.begin(9600);        // Start the serial communication at 9600 baud
+  pinMode(A0, INPUT);       // Set A0 as input for the potentiometer
+  pinMode(7, INPUT_PULLUP);  // Set pin 7 as input with pull-up resistor for the button
+  Serial.begin(9600);       // Start the serial communication at 9600 baud
 }
 
 void loop() {
-  if (digitalRead(7) == LOW) {  // Check if button is pressed (pin is LOW)
-    Serial.println(number);  // Send the current number to serial
-    delay(200);  // Debounce delay to avoid multiple readings
+  int potValue = analogRead(A0);  // Read the potentiometer value (0 - 1023)
+  int outputValue;
 
-    number++;  // Increment the number
-    if (number > 6) {  // Reset to 2 if it goes above 6
-      number = 2;
-    }
+  // Map the potentiometer reading to a range of 1 to 5
+  if (potValue < 205) {
+    outputValue = 1;
+  } else if (potValue < 410) {
+    outputValue = 2;
+  } else if (potValue < 615) {
+    outputValue = 3;
+  } else if (potValue < 820) {
+    outputValue = 4;
+  } else {
+    outputValue = 5;
+  }
+
+  // Only send the output value when the button is pressed
+  if (digitalRead(7) == LOW) {  // Check if button is pressed (pin is LOW)
+    Serial.println(outputValue + 1);  // Print the corresponding value
+    delay(500);  // Debounce delay to avoid multiple readings
   }
 }
